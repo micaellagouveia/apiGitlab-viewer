065d5975ebd690948b7e6e292b721a40f0f31923
Micaella Gouveia <micaella2212@gmail.com>
2020-04-15 18:56:26 -0300

    Add request url

--- src/requests/IssueRequest.js
+++ src/requests/IssueRequest.js
@@ -4,12 +4,12 @@ const axios = require('axios')
 module.exports = {
     closeIssue: (issue) => {
         const params = {
-            private_token : process.env.PRIVATE_TOKEN,
+            private_token : 'WqANVphpAfk8E3u1h4Qn',
             state_event : 'close'
         }
 
         return new Promise((resolve) => {
-            axios.put(`${process.env.GITLAB_API}/issues/${issue.iid}`, params).then((response) => {
+            axios.put(`https://git.cnj.jus.br/api/v4/projects/572/issues/${issue.iid}`, params).then((response) => {
                 resolve(response.data)
             }).catch((err) => {
                 resolve(err.response.data)
