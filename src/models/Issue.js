215bdd2bad5d40a2df668fac340d18c2e00d8804
Micaella Gouveia <micaella2212@gmail.com>
2020-04-15 19:19:44 -0300

    Fix gra,mmar error

--- src/models/Issue.js
+++ src/models/Issue.js
@@ -2,12 +2,12 @@ module.exports = class Issue {
   constructor(body) {
     this.id = body.object_attributes.id,
       this.iid = body.object_attributes.iid, //número da issue
-      //  this.title = body.object_attibutes.title,
+      this.title = body.object_attributes.title,
       this.state = body.object_attributes.state,
       this.project_id = body.project.id,
       this.project_name = body.project.name,
-      this.action = body.object_attributes.action
-    //  this.created_at = body.object_attibutes.created_at,
-    //  this.updated_at = body.object_attibutes.updated_at
+      this.action = body.object_attributes.action,
+      this.created_at = body.object_attributes.created_at,
+      this.updated_at = body.object_attributes.updated_at
   }
 }
