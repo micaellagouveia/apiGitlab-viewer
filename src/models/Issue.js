85b7fc1afa25d906d539097d9276cc8ca61995b1
Micaella Gouveia <micaella2212@gmail.com>
2020-04-14 21:24:41 -0300

    Fix title attribute

--- src/models/Issue.js
+++ src/models/Issue.js
@@ -2,7 +2,7 @@ module.exports = class Issue {
     constructor(body) {
         this.id = body.object_attributes.id,
         this.iid = body.object_attributes.iid, //número da issue
-        this.title = body.object_attibutes.title
+        this.title = body.object_attibutes.title,
         this.state = body.object_attributes.state,
         this.project_id = body.project.id,
         this.project_name = body.project.name,
